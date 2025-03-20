8b5170c201f17757870c7ccdab1559ed746ce2f2
ale-rt <alessandro.pisa@gmail.com>
2025-03-20 17:38:34 +0100

    WIP: fix on Plone 6.1

--- ftw/upgrade/jsonapi/zopeapp.py
+++ ftw/upgrade/jsonapi/zopeapp.py
@@ -1,7 +1,10 @@
 from AccessControl import getSecurityManager
+from AccessControl import Permissions
 from ftw.upgrade.jsonapi.base import APIView
 from ftw.upgrade.jsonapi.utils import action
 from ftw.upgrade.jsonapi.utils import jsonify
+from plone.base.interfaces import IPloneSiteRoot
+from ZODB.broken import Broken
 
 
 class ZopeAppAPI(APIView):
@@ -22,9 +25,31 @@ class ZopeAppAPI(APIView):
         """
         return getSecurityManager().getUser().getId()
 
+    def sites(self, root=None):
+        """ Copy the method in Products.CMFPlone.browser.admin.Overview.sites
+
+        The overview_view does not have this method anymore since Plone 6.1.
+        """
+        if root is None:
+            root = self.context
+
+        result = []
+        secman = getSecurityManager()
+        candidates = (
+            obj for obj in root.values() if not isinstance(obj, Broken)
+        )
+        for obj in candidates:
+            if obj.meta_type == "Folder":
+                result = result + self.sites(obj)
+            elif IPloneSiteRoot.providedBy(obj):
+                if secman.checkPermission(Permissions.view, obj):
+                    result.append(obj)
+            elif obj.getId() in getattr(root, "_mount_points", {}):
+                result.extend(self.sites(root=obj))
+        return result
+
     def _get_plone_sites(self):
-        overview_view = self.context.restrictedTraverse('plone-overview')
-        for site in overview_view.sites():
+        for site in self.sites():
             yield {'id': site.getId(),
                    'path': '/'.join(site.getPhysicalPath()),
                    'title': site.Title()}
